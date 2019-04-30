95fdb1b66ea911ccd00260e3a2047471d3de89e7
LuqJensen <lucas__jensen@hotmail.com>
2019-04-30 19:54:54 +0200

    Add project used to generate 5million strings

---
+++ 5million/5million/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace _5million
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            var strings = new string[5000000];
+            for (int i = 0; i < 5000000; ++i)
+            {
+                strings[i] = ((r.Next() % (5000000 / 4)) * 271828183).ToString();
+            }
+            File.WriteAllLines("5million.txt", strings);
+        }
+    }
+}
