fab6f34d5dbe695afafd662aa5363ab6dbf9efc8
RasiahT <rasiahmt@gmail.com>
2019-05-21 20:46:03 +0200

    Android: Init Android Project

---
+++ AndroidNative/app/src/main/java/dk/sdu/luctrasi/androidnative/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.sdu.luctrasi.androidnative;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
